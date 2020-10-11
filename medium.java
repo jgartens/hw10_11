fc61a4fec2dc28ccd6c888f6b76b7910147ec49d
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-10-11 15:04:02 -0500

    new hw

---
+++ medium.java
@@ -0,0 +1,30 @@
+/*
+Jacob Gartenstein HW 10_11 FTCI
+Java Control Flow Challenges Easy and Medium
+*/
+
+
+public class medium {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 5; i ++){
+            for (int k = 1; k < i + 2; k ++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            System.out.print("\n");
+            System.out.print("\n");
+        }
+    } 
+    
+
+
+
+    /*
+    The answer to the Easy Java Control Flow challenge is:
+    C. 20 -->
+    k = 80 at the beginning 
+    at the end, k = 80 - (12 * 5) = 20
+    */
+
+}
